6c85ab4b37acb823b905ace265123e6b9849d4b3
HarshJoshi123 <harsh2606joshi@gmail.com>
2021-12-27 20:54:18 +0530

    url fixed

--- src/apis/user.js
+++ src/apis/user.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 //const URL = 'http://localhost:8080'
-const URL = 'https://ycshop.herokuapp.com/'
+const URL = 'https://ycshop.herokuapp.com'
 const signup = (data)=>{
     return axios.post(`${URL}/signup`,{
         ...data
